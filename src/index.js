8ae2e1fe20bdfa5bd726c69bce2ddf26b1686d44
Amirul Fikri <amirul.rostam93@gmail.com>
2020-04-23 15:10:38 +0800

    tutorial 2

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-const App = () => {
-	return (
-		<div className="ui container comments">
-			<ApprovalCard>
-				<CommentDetail
-					author={faker.name.firstName()}
-					image={faker.image.avatar()}
-					comment={faker.lorem.text()}
-					date={faker.date.weekday()}
-				/>
-			</ApprovalCard>
-			<ApprovalCard>
-				<CommentDetail
-					author={faker.name.firstName()}
-					image={faker.image.avatar()}
-					comment={faker.lorem.text()}
-					date={faker.date.weekday()}
-				/>
-			</ApprovalCard>
-			<ApprovalCard>
-				<CommentDetail
-					author={faker.name.firstName()}
-					image={faker.image.avatar()}
-					comment={faker.lorem.text()}
-					date={faker.date.weekday()}
-				/>
-			</ApprovalCard>
-		</div>
-	);
-};
+class App extends React.Component {
+	state = { lat: null, errorMessage: "" };
+
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message })
+		);
+	}
+
+	componentDidUpdate() {
+		console.log("component did update");
+	}
+
+	renderContent() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>;
+		}
+
+		if (!this.state.errorMessage && this.state.lat) {
+			return <SeasonDisplay lat={this.state.lat} />;
+		}
+
+		return <Spinner message="Please accept location request" />;
+	}
+
+	render() {
+		return this.renderContent();
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
