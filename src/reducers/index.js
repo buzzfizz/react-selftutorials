20c8dd6364dd515ba1956be8a9e56dd7a09a6d85
Amirul Fikri <amirul.rostam93@gmail.com>
2020-04-25 22:46:01 +0800

    tut 5 with redux and thunk

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,35 +1,5 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 
-const songsReducer = () => {
-	return [
-		{
-			title: "No Scrubs",
-			duration: "4:05",
-		},
-		{
-			title: "No Scrubs 1",
-			duration: "3:05",
-		},
-		{
-			title: "No Scrubs 2",
-			duration: "2:05",
-		},
-		{
-			title: "No Scrubs 3",
-			duration: "1:05",
-		},
-	];
-};
-
-const selectedSongReducer = (selectedSong = null, action) => {
-	if (action.type === "SONG_SELECTED") {
-		return action.payload;
-	}
-
-	return selectedSong;
-};
-
-export default combineReducers({
-	songs: songsReducer,
-	selectedSong: selectedSongReducer,
-});
+export default combineReducers({ posts: postsReducer, users: usersReducer });
