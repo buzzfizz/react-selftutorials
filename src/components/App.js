f09a70786e07a6ca3cbf96c2ba32762b94855f6a
Amirul Fikri <amirul.rostam93@gmail.com>
2020-04-29 03:42:58 +0800

    refactor context api

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,19 @@
 import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageSelector from "./LanguageSelector";
-import LanguangeContext from "../contexts/LanguageContext";
+import { LanguageStore } from "../contexts/LanguageContext";
 import ColourContext from "../contexts/ColourContext";
 
 class App extends React.Component {
-	state = { language: "english" };
-
-	onLanguageChange = (language) => {
-		this.setState({ language });
-	};
-
 	render() {
 		return (
 			<div className="ui container">
-				<LanguageSelector onLanguageChange={this.onLanguageChange} />
-				<LanguangeContext.Provider value={this.state.language}>
+				<LanguageStore>
+					<LanguageSelector />
 					<ColourContext.Provider value="green">
 						<UserCreate />
 					</ColourContext.Provider>
-				</LanguangeContext.Provider>
+				</LanguageStore>
 			</div>
 		);
 	}
